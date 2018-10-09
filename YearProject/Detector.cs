d492be609f19d06cbaca969a07e6d883e6130190
Anthony <kolenic.anthony@gmail.com>
2018-10-09 22:10:47 +0200

    Added code for negativity

---
+++ YearProject/Detector.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YearProject
+{
+    class Detector
+    {
+        public Point Location { get; set; }
+        public int Radius { get; set; }
+        
+        public Detector()
+        {
+
+        }
+
+        public Detector(int x,int y, int r)
+        {
+            Location = new Point(x, y);
+            Radius = r;
+        }
+    }
+}
--- YearProject/GenrationWindow.xaml.cs
+++ YearProject/GenrationWindow.xaml.cs
@@ -37,10 +37,12 @@ namespace YearProject
             generatorThread.DoWork += new DoWorkEventHandler(Generator_DoWork);
             generatorThread.RunWorkerCompleted += new RunWorkerCompletedEventHandler(Generator_RunWorkerCompleted);
             generatorThread.ProgressChanged += new ProgressChangedEventHandler(Generator_ProgressChanged);
-            DataPass tmp = new DataPass();
-            tmp.settings = settings;
-            tmp.objects = contours;
-            tmp.inputData = features;
+            DataPass tmp = new DataPass
+            {
+                settings = settings,
+                objects = contours,
+                inputData = features
+            };
             generatorThread.RunWorkerAsync(tmp);
 
         }
@@ -71,6 +73,64 @@ namespace YearProject
 
             DataPass data = (DataPass)e.Argument;
             //e.Result = result;
+            Random randomGen = new Random(data.settings.Seed); ;
+            GaussianRandom gausRandomGen = new GaussianRandom(data.settings.Seed);
+
+            Dictionary<String, Detector[]> detectors = new Dictionary<string, Detector[]>();
+
+            //Generate detectors
+            foreach (String currentImage in data.inputData.Keys)
+            {
+                detectors.Add(currentImage, new Detector[data.settings.NumberOfNegativeDetectors]);
+                
+                for (int i = 0;i < data.settings.NumberOfNegativeDetectors;i++)
+                {
+                    int attempts = 0;
+                    bool found = false;
+                    while (!found && attempts < 10)
+                    {
+                        attempts++; // To prevent infinite loop
+                        //TODO (Anthony): Width and height should be individual per image
+                        int randomX = randomGen.Next(0, data.settings.Width);
+                        int randomY = randomGen.Next(0, data.settings.Height);
+                        //TODO (Anthony): Setting perhaps?
+                        int radius = randomGen.Next(10, 50);
+                        int doubleRadius = radius * radius;
+                        Detector newDetector = new Detector(randomX,randomY,radius);
+
+                        int numDetectSelf = 0;
+                        //check for collisions with points in each image
+
+                        foreach (System.Drawing.Point[] currentObjectList in data.inputData[currentImage].ToArrayOfArray()) // loop over each object in image
+                        {
+                            foreach (System.Drawing.Point point in currentObjectList) // loop over each point
+                            {
+                                //check if detect self
+                                if (Math.Pow(point.X - randomX,2) + Math.Pow(point.Y - randomY, 2) < doubleRadius)
+                                {
+                                    numDetectSelf++;
+                                }
+                            }
+                        }
+                        //TODO (Anthony): Detting for maximum self detection  
+                        if (numDetectSelf < 10)
+                        {
+                            found = true;
+                            detectors[currentImage][i] = newDetector;
+                        }
+
+                        /*
+                        //check for overlap with other detectors
+                        for (int k = 0; k < i;k++)
+                        {
+
+                        }
+                        */
+                    }
+                }
+            }
+            
+            
         }
 
 
